06cf36b4693c4e39daaf0498e82dc183e1cd6397
Ayoub <abassou643@gmail.com>
2022-02-14 16:25:16 +0100

    Update sql export Ayoub

--- db_export_ayoub.sql
+++ db_export_ayoub.sql
@@ -1,8 +1,29 @@
 -- Database export via SQLPro (https://www.sqlprostudio.com/allapps.html)
--- Exported by ayoubbassou at 14-02-2022 16:06.
+-- Exported by ayoubbassou at 14-02-2022 16:21.
 -- WARNING: This file may contain descructive statements such as DROPs.
 -- Please ensure that you are running the script at the proper location.
 
+
+-- BEGIN TABLE public.ipadress
+DROP TABLE IF EXISTS public.ipadress CASCADE;
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS public.ipadress (
+	"ipAdressID" bigint DEFAULT nextval('ipadress_sampleid_seq'::regclass) NOT NULL,
+	"userID" bigint NOT NULL,
+	navigator text NOT NULL,
+	"timeRequest" timestamp without time zone NOT NULL,
+	"numberTry" integer NOT NULL,
+	PRIMARY KEY("ipAdressID")
+);
+
+COMMIT;
+
+-- Table public.ipadress contains no data. No inserts have been genrated.
+-- Inserting 0 rows into public.ipadress
+
+
+-- END TABLE public.ipadress
 
 -- BEGIN TABLE public.users
 DROP TABLE IF EXISTS public.users CASCADE;
